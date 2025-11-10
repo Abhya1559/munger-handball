e76472b89197793f5548872765506ff2e1ad7d1b
abhya <abhya1559@gmail.com>
2025-11-10 19:58:31 +0530

    complete setup done now started working on backend

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div className="flex items-center justify-center min-h-screen">
+        <h1 className="font-extrabold bg-linear-to-bl from-violet-500 to-fuchsia-500 text-transparent bg-clip-text text-6xl font-stretch-expanded  ">
+          Work in Progress target 7 Days
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
