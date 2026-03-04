39d1e880b1f01d3d542283f0021527661d0d264b
abhya <abhya1559@gmail.com>
2026-03-04 21:32:09 +0530

    done some more changes

--- client/src/api/auth.api.ts
+++ client/src/api/auth.api.ts
@@ -1,15 +1,15 @@
 import api from "../api/axiosInstance";
 
-export const login = (data: any) => api.post("/players/login", data);
-export const register = (data: any) => api.post("/players/register", data);
-export const refreshToken = () => api.post("/players/refresh");
-export const getMe = () => api.get("/players/me", { withCredentials: true });
+export const login = (data: any) => api.post("/login", data);
+export const register = (data: any) => api.post("/register", data);
+export const refreshToken = () => api.post("/refresh");
+export const getMe = () => api.get("/me", { withCredentials: true });
 export const requestPasswordReset = (data: any) =>
-  api.post("/players/requestPasswordReset", data);
+  api.post("/requestPasswordReset", data);
 export const resetPassword = (
   id: string,
   token: string,
   data: { password: string },
-) => api.post(`/players/resetpassword/${id}/${token}`, data);
-export const logout = () => api.post("/players/logout");
+) => api.post(`/resetpassword/${id}/${token}`, data);
+export const logout = () => api.post("/logout");
 export const profile = () => api.get("/profile");
