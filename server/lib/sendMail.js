6a3648dd0f02e19f82d3469208af83012f5a5185
abhya <abhya1559@gmail.com>
2026-03-05 22:19:39 +0530

    done

--- server/lib/sendMail.js
+++ server/lib/sendMail.js
@@ -5,7 +5,8 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendEmail = async ({ to, subject, html }) => {
   try {
     const data = await resend.emails.send({
-      from: process.env.EMAIL,
+      //   from: process.env.EMAIL,
+      from: "Munger Handball <onboarding@resend.dev>",
       to,
       subject,
       html,
